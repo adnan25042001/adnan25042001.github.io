b825d198e33c58e1797e6d91a6115e3bf3fa0338
Adnan <adnan.hussain.136660@gmail.com>
2022-10-18 22:00:26 +0530

    added

--- theme.js
+++ theme.js
@@ -2,26 +2,26 @@
 let icon = document.querySelector("#theme-icon");
 
 if(localStorage.getItem("theme") == null){
-    localStorage.setItem("theme", "dark");
+    localStorage.setItem("theme", "light");
 }
 
 let localData = localStorage.getItem("theme");
 
 if(localData == "dark"){
     icon.src = "img/sun.png";
-    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
 }else if(localData == "light"){
     icon.src = "img/moon-solid-24.png";
-    document.body.classList.add("light-theme");
+    document.body.classList.remove("dark-theme");
 }
 
 icon.addEventListener("click",() => {
-    document.body.classList.toggle("light-theme");
-    if(document.body.classList.contains("light-theme")){
-        icon.src = "img/moon-solid-24.png";
-        localStorage.setItem("theme", "light");
-    }else{
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
         icon.src = "img/sun.png";
         localStorage.setItem("theme", "dark");
+    }else{
+        icon.src = "img/moon-solid-24.png";
+        localStorage.setItem("theme", "light");
     }
 });
