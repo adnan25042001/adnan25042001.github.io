9ef33529e86690fd252653402eb28b0c6dd34b38
Adnan <adnan.hussain.136660@gmail.com>
2022-11-07 21:59:00 +0530

    made responsive

--- index.js
+++ index.js
@@ -3,3 +3,43 @@ let header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     header.classList.toggle ("sticky", window.scrollY > 0);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () =>{
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("active");
+};
+
+window.onscroll = () =>{
+    menu.classList.remove("bx-x");
+    navlist.classList.remove("active");
+};
+
+const sr = ScrollReveal ({
+    distance: "45px",
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal(".home-text",{
+    delay: 350,
+    origin: "left"
+});
+
+sr.reveal(".home-img",{
+    delay: 350,
+    origin: "right"
+});
+
+sr.reveal(".sub-services, .about, .portfolio, .service, .cta, .contact",{
+    delay: 200,
+    origin: "bottom"
+});
+
+
+
+
+
+
