0a60de50171193797560c08e408685514577b2b4
Adnan <adnan.hussain.136660@gmail.com>
2022-12-10 21:03:01 +0530

    added

--- index.js
+++ index.js
@@ -17,29 +17,29 @@ window.onscroll = () =>{
     navlist.classList.remove("active");
 };
 
-const sr = ScrollReveal ({
-    distance: "45px",
-    duration: 2000,
-    reset: true
-})
+// const sr = ScrollReveal ({
+//     distance: "45px",
+//     duration: 2000,
+//     reset: true
+// })
 
-sr.reveal(".home-text, .about-img",{
-    delay: 350,
-    origin: "left"
-});
+// sr.reveal(".home-text, .about-img",{
+//     delay: 350,
+//     origin: "left"
+// });
 
-sr.reveal(".home-img, .about-text",{
-    delay: 350,
-    origin: "right"
-});
+// sr.reveal(".home-img, .about-text",{
+//     delay: 350,
+//     origin: "right"
+// });
 
-sr.reveal(".sub-services, .portfolio, .service, .cta, .contact",{
-    delay: 200,
-    origin: "bottom"
-});
-
-
+// sr.reveal(".sub-services, .portfolio, .service, .cta, .contact",{
+//     delay: 200,
+//     origin: "bottom"
+// });
 
 
-// email js need to search about that   
+
+
+// // email js need to search about that   
 
