70120b5bdccf182ad1b95d419a81fa1a1bd51d73
Adnan <adnan.hussain.136660@gmail.com>
2022-12-11 22:11:02 +0530

    added

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ window.onscroll = () =>{
     navlist.classList.remove("active");
 };
 
+let i = 0;
+
+let arr = ["#45b8ac", "#ff6b6b", "#2e86de", "#f368e0", "#efc050", "#e2244d"];
+
+function mainClr(){
+    if(i == arr.length) i = 0;
+    document.documentElement.style.setProperty("--main-clr", arr[i]);
+    i++;
+}
+
+setInterval(mainClr, 10000);
+
+// document.documentElement.style.setProperty("--main-clr", "red");
+
 // const sr = ScrollReveal ({
 //     distance: "45px",
 //     duration: 2000,
