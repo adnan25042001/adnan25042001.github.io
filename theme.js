9b1d046e021298b41ffd035d25e60e8d6776283e
Adnan <adnan.hussain.136660@gmail.com>
2023-06-07 17:45:00 +0530

    updated

--- theme.js
+++ theme.js
@@ -1,14 +1,13 @@
-
 let arr = ["#e2244d", "#45b8ac", "#ff6b6b", "#2e86de", "#f368e0", "#efc050"];
 
 let i = +localStorage.getItem("i") || 0;
-if(i >= arr.length) i = 0;
+if (i >= arr.length) i = 0;
 document.documentElement.style.setProperty("--main-clr", arr[i]);
 i = i + 1;
 
-function mainClr(){
-    if(i >= arr.length) i = 0;
-    localStorage.setItem("i",i);
+function mainClr() {
+    if (i >= arr.length) i = 0;
+    localStorage.setItem("i", i);
     document.documentElement.style.setProperty("--main-clr", arr[i]);
     i = i + 1;
 }
