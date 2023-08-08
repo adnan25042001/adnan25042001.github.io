a072c1fa0f40a3c90503bfe77b5d050af156ab56
Adnan <adnan.hussain.136660@gmail.com>
2023-08-08 14:24:17 +0530

    updated

--- src/components/GithubCalender.jsx
+++ src/components/GithubCalender.jsx
@@ -5,6 +5,10 @@ import GitHubCalendar from "react-github-calendar";
 const GithubCalender = () => {
     return (
         <section id="github">
+            <div className="heading">
+                <h3 className="text-mainColor">Github</h3>
+                <h2>Calander & Stats</h2>
+            </div>
             <div className="m-auto flex flex-col items-center justify-center gap-10">
                 <motion.div
                     initial={{ opacity: 0, scale: 0 }}
@@ -23,7 +27,7 @@ const GithubCalender = () => {
                     className="px-6"
                 >
                     <img
-                        className="border border-gray-400 rounded-md shadow-sm shadow-mainColor hover:scale-110 transition-all duration-300 hover:shadow-md hover:shadow-mainColor"
+                        className="border border-gray-400 rounded-md shadow-md shadow-mainColor hover:scale-110 transition-all duration-300 hover:shadow-lg hover:shadow-mainColor"
                         src="https://github-readme-streak-stats.herokuapp.com?user=adnan25042001&amp;theme=transparent&amp;hide_border=true&amp;ring=8d2de2&amp;background=020912&amp;currStreakLabel=8d2de2&amp;fire=FF0000&amp;currStreakNum=ffffff&amp;sideNums=ffffff&amp;currStreakLabel=ffffff&amp;sideLabels=ffffff&amp;dates=e4e4e4"
                         alt=""
                     />
@@ -37,7 +41,7 @@ const GithubCalender = () => {
                     className="px-6"
                 >
                     <img
-                        className="border border-gray-400 rounded-md shadow-sm shadow-mainColor hover:scale-110 transition-all duration-300 hover:shadow-md hover:shadow-mainColor"
+                        className="border border-gray-400 rounded-md shadow-md shadow-mainColor hover:scale-110 transition-all duration-300 hover:shadow-lg hover:shadow-mainColor"
                         src="https://github-readme-stats.vercel.app/api?username=adnan25042001&amp;show_icons=true&amp;locale=en&amp;theme=tranparent&amp;hide_border=true&amp;text_color=ffffff&amp;title_color=8d2de2&amp;bg_color=020912&amp;icon_color=8d2de2"
                         alt=""
                     />
@@ -51,7 +55,7 @@ const GithubCalender = () => {
                     className="px-6"
                 >
                     <img
-                        className="border border-gray-400 rounded-md shadow-sm shadow-mainColor hover:scale-110 transition-all duration-300 hover:shadow-md hover:shadow-mainColor"
+                        className="border border-gray-400 rounded-md shadow-md shadow-mainColor hover:scale-110 transition-all duration-300 hover:shadow-lg hover:shadow-mainColor"
                         src="https://github-readme-stats.vercel.app/api/top-langs/?username=adnan25042001&amp;theme=transparent&amp;text_color=ffffff&amp;hide_border=true&amp;title_color=8d2de2"
                     />
                 </motion.a>
